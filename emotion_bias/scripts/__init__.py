bd14c47872065c1c0414d540c6f61128d94e03ae
Mo Zaman <74645615+kamruzzaman15@users.noreply.github.com>
2026-05-07 00:59:26 -0400

    Delete emotion_bias directory

--- emotion_bias/scripts/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Utility scripts for running experiments in the Emotion-Steered Reasoning project.
-
-This file marks the scripts directory as a Python package. Most scripts in this
-folder are intended to be run from the command line.
-"""
-
-__all__ = []
